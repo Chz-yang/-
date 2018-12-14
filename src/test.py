2b192453ad9959c211fc4f026dcd5be305a84eb4
yang <yangchz3@mail2.sysu.edu.cn>
2018-12-14 18:05:04 +0800

    change test.py

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import pyodbc
 conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=127.0.0.1;port=1433;DATABASE=TPCH;UID=experiment;PWD=Yy7758258')
 
 cursor = conn.cursor()
-cursor.execute("SELECT orderkey FROM Orders")
+cursor.execute("SELECT * FROM Orders")
 row = cursor.fetchall()
 if row:
-    print(row)
+    print(row[0:10])

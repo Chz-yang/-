23c2e722ab04d1034027421d3af72a335c6ddde2
zhangjintao <gdzhangjintao@163.com>
2018-12-24 14:43:54 +0800

    update supplier

--- src/supplier.py
+++ src/supplier.py
@@ -14,6 +14,7 @@ class Supplier():
         self.id = id
         self.cursor = cursor
         self.quit = False
+        self.lock = threading.Lock()
         self.todoOrders = {}
 
     def activity(self):
@@ -22,13 +23,10 @@ class Supplier():
             option = input("\n>> 'c' to check Orders; 'd' to add dishes\n"
                            ">>  'a' to add contact; 'q' to quit: ")
             if option == 'c':  # check orders
-                if len(self.todoOrders) == 0:
-                    print("\n>> There are no orders to handle now.")
-                else:
-                    pass
+                self.handleOrders()
 
             elif option == 'd':   # add dishes
-                pass
+                self.addDishes()
 
             elif option == 'a':   # add contacts
                 self.addContact()
@@ -37,6 +35,58 @@ class Supplier():
                 self.quit = True
                 return False
         return True
+
+    def handleOrders(self):
+        ''' handle orders to to delivering '''
+        self.lock.acquire()
+        if len(self.todoOrders) == 0:
+            print("\n>> There are no orders to handle now.")
+        else:
+            for order_id in self.todoOrders:
+                self.cursor.execute('''
+                                    UPDATE Orders set Orders.state ='to_delivering'
+                                    where Orders.order_id =  ''', order_id[0])
+                self.cursor.commit()
+            
+        self.lock.release()
+        pass
+
+    def addDishes(self):
+        ''' add dishes '''
+        self.showDishes()
+
+        while True:
+            choice = input("\n>> 'a' to add new dishes; 'q' to quit:")
+            if choice == 'a':
+                features = ["dishes name", "retail price", "comment"]  # 输入属性
+                values = [None, None, None]  # 输入值(密码、姓名、电话号码)
+                no = 0  # 输入顺序
+                while no < 3:
+                    values[no] = validInput(features[no], no)
+                    if values[no] == 'b': # 回退到上一步
+                        no = no - 1
+                    elif values[no] == 'q':   # 退出输入
+                        return
+                    else:
+                        no = no + 1
+
+                self.cursor.execute("SELECT count(*) FROM Dishes")
+                count = cursor.fetchone()
+                dishes_id = str(count[0]) # 假设contact ID从0开始
+
+                self.cursor.execute('''
+                                    insert into Dishes values
+                                    (?, ?, ?, ?)''',
+                                    dishes_id, values[0], values[1], 0, values[2])
+                self.cursor.commit()
+
+                self.cursor.execute('''
+                                    insert into Supp_Dishes values
+                                    (?, ?)''', self.id, dishes_id)
+                self.cursor.commit()
+
+            elif choice == 'q':
+                return
 
     def addContact(self):
         self.showContact()
@@ -73,6 +123,23 @@ class Supplier():
             elif choice == 'q':
                 return
 
+    def showDishes(self):
+        ''' show current dishes '''
+        self.cursor.execute('''
+                            select *
+                            from Dishes
+                            where contact_id in
+                                (select dishes_id
+                                from Supp_Dishes
+                                where supp_id = ?)''', self.id)
+        dishes = self.cursor.fetchall()
+        if len(dishes) == 0:
+            print("\n>> You don't have any dishes now!")
+        else:
+            print('\n>> Here are you current Dishes:')
+            for dish in dishes:
+                print('>>> dish name:', dish[1], 'price:', dish[2], 'sales:', dish[3], 'comments:', dish[4])
+
     def showContact(self):
         ''' show current contact '''
 
@@ -91,20 +158,19 @@ class Supplier():
             for contact in contacts:
                 print('>>> tele:', contact[1], 'addr:', contact[2], 'recvName:', contact[3])
 
-    # def getContact(self):
-    #     ''' get supplier's contact '''
-    #     self.cursor.execute(''' 
-    #                         select contact_id
-    #                         from Supp_Contact
-    #                         where supp_id = ?''', self.id)
-
     def checkOrders(self):
-        # while True:
-        #     self.cursor.execute('''
-        #                         select * 
-        #                         from Orders
-        #                         where ''')
-        pass
+        ''' check orders per 10 seconds '''
+        while True:
+            time.sleep(10)
+            self.cursor.execute('''
+                                select *
+                                from Supp_Orders, Orders
+                                where  Supp_Orders.supp_id = ? and
+                                       Supp_Orders.order_id = Orders.order_id and
+                                       Orders.state = ?''', self.id, 'to_do')
+            self.lock.acquire()
+            self.todoOrders = self.cursor.fetchall()
+            self.lock.release()
 
 
 def validInput(feature, no):
